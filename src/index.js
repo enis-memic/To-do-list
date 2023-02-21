3552b77d0d85716554b8504abd3870b0c65cf36d
Enis <enis.mema@gmail.com>
2023-02-21 15:49:32 +0100

    Update list-structure

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import _ from 'lodash';
+import './style.css';
+
+const tasks = [
+  {
+    description: 'wash the dishes',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'complete to do list project',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'workout',
+    completed: true,
+    index: 3,
+  },
+];
+const taskList = () => {
+  const container = document.getElementById('data');
+  for (let i = 0; i < tasks.length; i++) {
+    const task = document.createElement('div');
+    task.classList.add('list-to-do');
+    task.innerHTML = `
+    <div class="list-el">
+    <input type="checkbox">
+    <p>${tasks[i].description}</p>
+    </div>
+    `;
+    container.appendChild(task);
+  }
+};
+taskList();
