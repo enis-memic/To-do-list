7d2cce6d890f300bb8b73fbbb59ef312283f256f
Enis <enis.mema@gmail.com>
2023-02-22 14:50:41 +0100

    Add remove function

--- src/crud.js
+++ src/crud.js
@@ -1,4 +1,4 @@
-const taskListDisplay = document.getElementById('data');
+const taskListDisplay = document.querySelector('#data');
 const inputValue = document.querySelector('.new-task');
 const addButton = document.querySelector('.add-to-list');
 
@@ -11,22 +11,60 @@ const display = () => {
     const task = document.createElement('div');
     task.classList.add('task');
     task.innerHTML = `
-    <input type:"checkbox">
+    <input type=:"checkbox" >
     <input class:"edit" type=:"text" value="${element.description}">
     <div>
     <i id="${element.index}" class="fa-solid fa-trash-can"></i>
     </div>
     `;
-    taskListDisplay.appendChild('task');
+    taskListDisplay.appendChild(task);
     const taskList2 = task.children[1];
     taskList2.addEventListener('change', () => {
-      const listingData = document.getElementById('data');
+      const listingData = document.querySelector('#data');
       const arrayList = Array.from(listingData.children);
       const index = arrayList.indexOf(task);
       const taskLocal = JSON.parse(localStorage.getItem('localItem'));
       taskLocal[index].description = taskList2.value;
-      localStorage.setItem('localItem', JSON.stringify('taskLocal'));
+      localStorage.setItem('localItem', JSON.stringify(taskLocal));
     });
   });
 };
+
 display();
+
+addButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (inputValue.value === '') return;
+  taskList = JSON.parse(localStorage.getItem('localItem')) || [];
+
+  const object = {
+    description: inputValue.value,
+    index: taskList.length,
+    completed: false,
+  };
+  taskList.push(object);
+  localStorage.setItem('localItem', JSON.stringify(taskList));
+
+  inputValue.value = '';
+  display();
+});
+
+const remove = (index) => {
+  const removeList = taskList.filter((element) => element.index !== index);
+  taskList.length = 0;
+  let i = 0;
+  removeList.forEach((element) => {
+    element.index = i;
+    i += 1;
+  });
+  taskList.push(...removeList);
+  localStorage.setItem('localItem', JSON.stringify(taskList));
+  display();
+};
+
+taskListDisplay.addEventListener('click', (e) => {
+  if (e.target.classList.contains('fa-solid')) {
+    const index = parseInt(e.target.getAttribute('id'), 10);
+    remove(index);
+  }
+});
