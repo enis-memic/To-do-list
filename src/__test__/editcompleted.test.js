928c7389f15a7c03bca3aebc3942d941cd195b50
Mugisha Innocent <tafloninno@gmail.com>
2023-03-02 13:17:10 +0300

    Update editcompleted.test.js

--- src/__test__/editcompleted.test.js
+++ src/__test__/editcompleted.test.js
@@ -32,3 +32,14 @@ describe('Test for update task lists', () => {
     ]);
   });
 });
+describe('Test for completed list task', () => {
+  it('Check completed list task is removed or not', () => {
+    localStorage.removeItem('localItem');
+    addList('task 1');
+    completedList(0);
+    const before = JSON.parse(localStorage.getItem('localItem'));
+    clear();
+    const after = JSON.parse(localStorage.getItem('localItem'));
+    expect(after.length).toBe(before.length - 1);
+  });
+});
